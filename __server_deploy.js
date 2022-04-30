3f1ad90b2ccdefd2158461934cdef416d2ae3031
Hamidul-1 <hamidul786cse@gmail.com>
2022-04-30 12:39:31 +0600

    added hero end point

--- __server_deploy.js
+++ __server_deploy.js
@@ -9,6 +9,9 @@
  * ------------------------
  * For each project one time
  * -------------------------
- * 1. 
- * 
+ * 1. heroku create
+ * 2. make sure you: git add . git commit. git push
+ * 3. git push heroku main
+ * 4. Go To Heroku Dashboard > Current Project > Setting > Reveal Config vars
+ * 5. 
  */
